56697d7dfa605eff09be62ed7051deeec97d2a16
CHEN Xian-an <xianan.chen@gmail.com>
2021-12-12 20:14:38 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# consult-project
+
+Consult integration for porject.
+
+## Usage
+
+### Install with [use-package](https://github.com/jwiegley/use-package)/[straight.el](https://github.com/raxod502/straight.el):
+
+```elisp
+(use-package consult-project
+  :straight (consult-project :type git :host github :repo "cxa/consult-project")
+  :custom (consult-project-recentf-max-projects 5)
+  :bind ("M-p" . consult-project))
+```
+
+### or clone to your `load-path`.
---
+++ consult-project.el
@@ -0,0 +1,165 @@
+;;; consult-project.el --- Consult integration for porject  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021
+
+;; Author:  CHEN Xianan
+;; URL: https://github.com/cxa/consult-project
+;; Keywords: consult, project
+;; Version: 0.1
+;; Package-Requires: (project consult recentf subr-x dash)
+
+;; This file is NOT part of GNU Emacs.
+
+;;; License:
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; A multi-view for opened buffers and files within project.
+;;
+;; Just run the `consult-project' and/or bind it to a hotkey.
+;;
+;; To filter the multiview use:
+;; B - For buffers inside `project-current'
+;; F - For files inside `project-current'
+;; R - For recent opened projects
+;; P - For all known projects
+
+;;; Code:
+
+(require 'project)
+(require 'consult)
+(require 'recentf)
+(require 'subr-x)
+(require 'dash)
+
+(defcustom consult-project-sources
+  '(consult-project--source-buffer
+    consult-project--source-file
+    consult-project--source-project-recentf
+    consult-project--source-project-all)
+  "`consult-project' sources for `consult--multi'."
+  :type '(repeat symbol)
+  :group 'consult-project)
+
+(defcustom consult-project-recentf-max-projects 5
+  "Max recentf projects"
+  :type 'integer
+  :group 'consult-project)
+
+(defvar consult-project--history nil)
+
+(defun consult-project--choose-file (proj-root-dir)
+  "File list for PROJ-ROOT-DIR."
+  (let* ((inv-root (propertize proj-root-dir 'invisible t))
+         (files (mapcar (lambda (f) (string-remove-prefix proj-root-dir f))
+                        (project-files (project--find-in-directory proj-root-dir)))))
+    (mapcar (lambda (f) (concat inv-root f)) files)))
+
+(defun consult-project--file (selected-dir)
+  "Create a view for project in SELECTED-DIR."
+  (find-file (consult--read
+              (consult-project--choose-file (expand-file-name selected-dir))
+              :prompt "Switch file: "
+              :sort t
+              :require-match t
+              :category 'file
+              :state (consult--file-preview)
+              :history 'file-name-history)))
+
+(defun consult-project--recentf-projects ()
+  "Return list of recentf projects"
+  (let* ((get-proj (-compose #'project--find-in-directory #'file-name-directory))
+         (projs (remq nil (mapcar get-proj recentf-list)))
+         (dirs (delete-dups (mapcar (-compose #'abbreviate-file-name  #'project-root) projs))))
+    (-take consult-project-recentf-max-projects dirs)))
+
+(defun consult-project--current-project-name ()
+  (when-let (pc (project-current))
+    (consult--project-name (project-root pc))))
+
+(defvar consult-project--source-buffer
+  `(
+    :name     "Project Buffer"
+    :narrow   ?p
+    :category buffer
+    :face     consult-buffer
+    :history  buffer-name-history
+    :state    ,#'consult--buffer-state
+    :enabled  ,(lambda () (project-current))
+    :items
+    ,(lambda ()
+       (when-let (root-dir (expand-file-name (project-root (project-current))))
+         (mapcar #'buffer-name
+                 (seq-filter (lambda (x)
+                               (when-let (file (buffer-file-name x))
+                                 (string-prefix-p root-dir file)))
+                             (consult--buffer-query :sort 'visibility)))))))
+
+
+(defvar consult-project--source-file
+  `(
+    :name      "Project File"
+    :narrow    ?f
+    :category  project-file
+    :face      consult-file
+    :history   file-name-history
+    :action    ,(lambda (f) (consult--file-action (concat (project-root (project-current)) f)))
+    :enabled   ,(lambda () (project-current))
+    :items
+    ,(lambda ()
+       (let ((root-dir (expand-file-name (project-root (project-current)))))
+         (mapcar
+          (lambda (f) (string-remove-prefix root-dir f))
+          (project-files (project-current)))))))
+
+(defvar consult-project--source-project-recentf
+  `(
+    :name     "Recent Projects"
+    :narrow   ?r
+    :category file
+    :face     consult-file
+    :history  file-name-history
+    :enabled  ,(lambda () recentf-mode)
+    :action   ,#'consult-project--file
+    :items    ,#'consult-project--recentf-projects))
+
+(defvar consult-project--source-project-all
+  `(
+    :name     "All Projects"
+    :narrow   ?p
+    :category file
+    :face     consult-file
+    :history  file-name-history
+    :enabled  ,(lambda() t)
+    :action   ,#'consult-project--file
+    :items    ,#'project-known-project-roots))
+
+;;;###autoload
+(defun consult-project ()
+  "A consult multi view with project integration."
+  (interactive)
+  (when-let (buffer (consult--multi consult-project-sources
+                                    :prompt "Switch: "
+                                    :require-match t
+                                    :history consult-project--history
+                                    :sort nil))
+    (unless (cdr buffer)
+      (funcall consult--buffer-display (car buffer)))))
+
+(provide 'consult-project)
+;;; consult-project.el ends here
